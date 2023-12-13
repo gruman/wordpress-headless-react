c5590ab0f186429b67ee0de3d91416f165a405ea
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-12-13 04:50:41 -0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, ListGroup } from 'react-bootstrap';
+
+import axios from 'axios';
+
+interface Post {
+  title: string;
+  link: string;
+  id: number;
+}
 
 function App() {
+
+  const [posts, setPosts] = useState<Post[]>();
+  useEffect(() => {
+    axios.get("https://wordpress.matthewgruman.com/wp-json/wp/v2/posts")
+      .then(results => {
+        let newPosts = [];
+        for (let i = 0; i < results.data.length; i++) {
+          const newPost : Post = {
+            id: results.data[i].id,
+            link: results.data[i].link,
+            title: results.data[i].title.rendered
+          }
+          newPosts.push(newPost);
+        }
+        console.log(newPosts)
+        setPosts(newPosts)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Headless Wordpress</h1>
+      <p>by <a href="https://matthewgruman.com">Matthew Gruman</a></p>
+      <h2>Posts</h2>
+      <ListGroup>
+        {
+          posts &&
+          posts.map(item => {
+            return (
+              <ListGroup.Item key={item.id}>
+                <a href={item.link}>{item.title}</a>
+              </ListGroup.Item>
+            )
+          })
+        }
+      </ListGroup>
+    </Container>
   );
 }
 
